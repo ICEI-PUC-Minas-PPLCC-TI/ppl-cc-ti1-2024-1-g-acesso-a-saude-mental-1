ee8da6083f930d44a61c2434a99ea80c15097c53
RToffu <86581240+RToffu@users.noreply.github.com>
2024-05-19 15:10:04 -0300

    Update ppisicologo.js

--- codigo/assets/js/ppisicologo.js
+++ codigo/assets/js/ppisicologo.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('save-button').addEventListener('click', function() {
     saveProfile();
 });
-
+// Menu hamburguer js
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -24,8 +24,9 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+//END HAMBURGUER
 
-
+//EDICAO DOS CAMPOS DO BODY
 function editField(fieldId) {
     const field = document.getElementById(fieldId);
     field.disabled = false;
@@ -40,47 +41,52 @@ function saveProfile() {
     const biografia = document.getElementById('biografia').value;
     const localizacao = document.getElementById('localizacao').value;
     const especialidades = document.getElementById('especialidades').value;
+    const contatos = document.getElementById('contatos').value;
     const foto = document.getElementById('foto').files[0];
 
     const reader = new FileReader();
     reader.onloadend = function() {
         const fotoDataUrl = reader.result;
 
-        const data = {
+        const data = { //CONST DATA REGISTRO (SERA ALTERADO POR ARQUIVO .JSON SEPARADO NA INTEGRACAO DAS PAGINAS)
             nome: nome,
             biografia: biografia,
             localizacao: localizacao,
             especialidades: especialidades,
+            contatos: contatos,
             foto: fotoDataUrl
         };
 
-        localStorage.setItem('profileData', JSON.stringify(data));
+        localStorage.setItem('profileData', JSON.stringify(data)); //LOCAL STORAGE (ALTERAR ENTREGA FINAL)
         alert('Perfil atualizado com sucesso!');
+        location.reload();
     };
 
     if (foto) {
         reader.readAsDataURL(foto);
     } else {
-        const data = {
+        const data = { ////CONST DATA REGISTRO (SERA ALTERADO POR ARQUIVO .JSON SEPARADO NA INTEGRACAO DAS PAGINAS)
             nome: nome,
             biografia: biografia,
             localizacao: localizacao,
             especialidades: especialidades,
+            contatos: contatos,
             foto: document.getElementById('profile-pic').src
         };
 
-        localStorage.setItem('profileData', JSON.stringify(data));
+        localStorage.setItem('profileData', JSON.stringify(data)); //LOCAL STORAGE
         alert('Perfil atualizado com sucesso!');
     }
 }
 
-function loadProfile() {
-    const data = JSON.parse(localStorage.getItem('profileData'));
+function loadProfile() { //CARREGA AS PAGINAS DO LOCAL STORAGE
+    const data = JSON.parse(localStorage.getItem('profileData')); // CONTEUDO PODE SER DELETADOS AO LIMPAR LOCAL STORAGE DO NAVEGADOR
     if (data) {
         document.getElementById('nome').value = data.nome;
         document.getElementById('biografia').value = data.biografia;
         document.getElementById('localizacao').value = data.localizacao;
         document.getElementById('especialidades').value = data.especialidades;
+        document.getElementById('contatos').value = data.contatos;
         document.getElementById('profile-pic').src = data.foto;
     }
 }
