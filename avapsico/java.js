2770730ffe8b81c3a7d19c5fde74ae93ee193709
olveirancc <166147664+olveirancc@users.noreply.github.com>
2024-06-15 19:20:25 -0300

    Delete avapsico directory

--- avapsico/java.js
+++
@@ -1,8 +0,0 @@
-const psicoData = {
-    nome: "Vania Aparecida",
-    photo: "assets/default-avatar.png",
-}
-
-localStorage.setItem('psicoData', JSON.stringify(psicoData));
-var storedData = JSON.parse(localStorage.getItem('psicoData'));
-document.getElementById('psiconome').textContent = `Nome: ${storedData.nome}`;
