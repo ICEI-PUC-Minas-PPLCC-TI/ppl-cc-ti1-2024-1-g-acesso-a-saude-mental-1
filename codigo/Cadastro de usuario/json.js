687f19323cd9c15c076b7889948c68eae0731e76
Mateus Viggiani <mateus.viggiani@gmail.com>
2024-06-19 09:30:13 -0300

    a

--- codigo/Cadastro de usuario/json.js
+++ codigo/Cadastro de usuario/json.js
@@ -4,9 +4,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 if (isset($data['nome']) && isset($data['email']) && isset($data['senha']) &&
     isset($data['cpf']) && isset($data['endereco'])) {
-  
     echo json_encode(['success' => true]);
 } else {
-
     echo json_encode(['success' => false, 'message' => 'Todos os campos são obrigatórios.']);
 }
