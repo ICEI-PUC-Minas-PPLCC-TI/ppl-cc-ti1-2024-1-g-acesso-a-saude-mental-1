85327abf2499927e252a48effee64d3718471ec9
olveiriancc <jorge.amado.ncc@gmail.com>
2024-07-03 15:28:04 -0300

    tela psicologos v1

---
+++ codigo/assets/js/procurarPsicologo.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/codigo/assets/json/psicologos.json')
+        .then(response => response.json())
+        .then(psicologos => {
+            const cardsContainer = document.getElementById('cards-container');
+            psicologos.forEach(psicologo => {
+                const cardHTML = `
+                    <div class="card">
+                        <img src="${psicologo.imagem}" alt="">
+                        <div class="card-content">
+                            <h2 id="psicologotext1">${psicologo.nome}</h2>
+                            <p id="psicologotext2">${psicologo.especialidade}</p>
+                            <p id="psicologotext2">${psicologo.descricao}</p>
+                            <p id="psicologotext2"><a href="agendaPaciente.html">Marcar consulta</a></p>
+                        </div>
+                    </div>
+                `;
+                cardsContainer.innerHTML += cardHTML;
+            });
+        })
+        .catch(error => console.error('Erro ao carregar psicólogos:', error));
+});
